28adc3619d97ccd35284421465b97570bd899538
yfchua <35942337+yfchua@users.noreply.github.com>
2022-06-09 01:24:22 +0800

    Update jsonserver.py

--- jsonserver.py
+++ jsonserver.py
@@ -1,16 +1,19 @@
+#!/home/admin/flask_app/bin/python3
 # import main Flask class and request object
 from flask import Flask, request
 import time,os,re,subprocess,threading
+import configparser
 
 # create the Flask app
 app = Flask(__name__)
 
-def run_shell_command(commandline, timeout):
+def run_shell_command(commandline, h, timeout):
     """
     Run OS command with timeout and status return. Also works in Python 2.7.
     Example:
     output, task_return_code, task_timeouted = run_shell_command('shell command text', timeout)
     """
+    print(h)
     output = ""
     task_timeouted = False
     task_return_code = 100
@@ -65,26 +68,59 @@ def run_shell_command(commandline, timeout):
 
 @app.route('/alerttrigger', methods=['POST'])
 def alerttrigger():
+    config = configparser.ConfigParser()
+    config.read('test.conf')
+    sections = config.sections()
+
     request_data = request.get_json()
     for i in range(len(request_data["alerts"])):
        a = request_data["alerts"][i]["labels"]["alertname"]
        ii = request_data["alerts"][i]["labels"]["instance"]
+       desc = request_data["alerts"][i]["annotations"]["description"]
        print("alertname: ",a)
        print("hostname: ",ii)
+       print("description: ", desc)
        if a == "HostOutOfDiskSpace":
            o = request_data["alerts"][i]["labels"]["mountpoint"]
            print("option: ", o)
 
        print("-----------------------------")
        if a == "HostOutOfDiskSpace":
-           r = run_shell_command("/opt/alertmon/python/bin/flask/bin/alert_trigger.sh " + a + " disk " + o , 60)
-       if a == "HighCPU":
-           r = run_shell_command("/opt/alertmon/python/bin/flask/bin/alert_trigger.sh " + a + " cpu" , 60)
-       if a == "HighMEM":
-           r = run_shell_command("/opt/alertmon/python/bin/flask/bin/alert_trigger.sh " + a + " mem" , 60)
+           for section in sections:
+               n = config[section]['node']
+               if n == "ANY" or  n == ii:
+                   if config[section]["alert"] == a:
+                       if config[section]["action"] == "exec_remote":
+                           exec_cmd = config[section]["option"]
+                           print("echo  " +  section + " " + a + " " + exec_cmd + " " + o + " " +  ii)
+                       if config[section]["action"] == "forward_email":
+                           print("echo  formward_email" + " " + desc)
+
+       if a == "HostHighCpuLoad":
+           for section in sections:
+               n = config[section]['node']
+               if n == "ANY" or n == ii:
+                   if config[section]["alert"] == a:
+                       if config[section]["action"] == "exec_remote":
+                           exec_cmd = config[section]["option"]
+                           print("echo  " + section + " " + a + " " + exec_cmd + " " +  ii)
+                       if config[section]["action"] == "forward_email":
+                           print("echo  formward_email" + " " + desc)
+
+       if a == "HostOutOfMemory":
+           for section in sections:
+               n = config[section]['node']
+               if n == "ANY" or  node == ii:
+                   if config[section]["alert"] == a:
+                       if config[section]["action"] == "exec_remote":
+                           exec_cmd = config[section]["option"]
+                           print("echo  " + section + " " + a + " " + exec_cmd + " " + ii)
+                       if config[section]["action"] == "forward_email":
+                           print("echo  formward_email" + " " + desc)
+
 
     return ""
 
 if __name__ == '__main__':
     # run app in debug mode on port 5000
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=5001)
